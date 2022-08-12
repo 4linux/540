97220f77068b93d3b09878cdbaeeb67919c415f0
Roberto Farias <roberto.farias@4linux.com.br>
2022-08-12 19:40:18 -0300

    Update files

---
+++ site/index.php
@@ -0,0 +1,66 @@
+<?php
+require('header.php');
+?>
+	<div id="grid_content">
+		<div class="main_content">
+			<?php require('menu.php'); ?>
+			<h1>Coletas em Andamento</h1>
+			<table cellpadding="0" cellspacing="0" border="0" class="tabela">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Pedido</th>
+						<th>Cliente</th>
+						<th>Telefone</th>
+						<th>Coleta</th>
+						<th>Valor</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td>99872</td>
+						<td><a href="4linux.php">4Linux</a></td>
+						<td>(11) 2125-4747</td>
+						<td>Apostilas</td>
+						<td>120,00</td>
+						<td>Aguardando Carro</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td>99873</td>
+						<td><a href="cliente.php">Dee Dee Turismo</a></td>
+						<td>(11) 2125-4747</td>
+						<td>Malote Banco</td>
+						<td>30,00</td>
+						<td>Aguardando Carro</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td>99874</td>
+						<td><a href="cliente.php">Levinsky Corretora</a></td>
+						<td>(11) 2125-4747</td>
+						<td>Cartorio</td>
+						<td>30,00</td>
+						<td>Em viagem</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td>99875</td>
+						<td><a href="cliente.php">Mordechai Advogados</a></td>
+						<td>(11) 2125-4747</td>
+						<td>Docs TST</td>
+						<td>30,00</td>
+						<td>Em viagem</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		
+<?php require('sidebar.php'); ?>
+</div>
+
+<?php
+require('footer.php');
+?>
---
+++ site/supervisora.php
@@ -0,0 +1,253 @@
+<?php
+require('header.php');
+?>
+	<div id="grid_content">
+		<div class="main_content">
+			<?php require('menu.php'); ?>
+			<h1>Clientes a repassar</h1>
+			<table cellpadding="0" cellspacing="0" border="0" class="tabela">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Nome</th>
+						<th>Email</th>
+						<th>Telefone</th>
+						<th>Resp.</th>
+						<th>Status</th>
+						<th>Interesses</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="balaio">
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>412, 444</td>
+					</tr>
+					<tr class="balaio">
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>450, 451, 452</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Cotinha</td>
+						<td>Novo</td>
+						<td>412, 444, 490</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr class="duplicado">
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+				</tbody>
+			</table>
+			<p class="acoes">
+				<label>Ações: </label>
+				<select>
+					<option>Unir</option>
+					<option>Alterar dono</option>
+					<option>Enviar email</option>
+					<option>Exportar</option>
+				</select>
+			</p>
+			
+			<h1>Clientes em reciclagem</h1>
+			<table cellpadding="0" cellspacing="0" border="0" class="tabela">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Nome</th>
+						<th>Email</th>
+						<th>Telefone</th>
+						<th>Resp.</th>
+						<th>Status</th>
+						<th>Interesses</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>412, 444</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>450, 451, 452</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Cotinha</td>
+						<td>Novo</td>
+						<td>412, 444, 490</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+				</tbody>
+			</table>
+			<p class="acoes">
+				<label>Ações: </label>
+				<select>
+					<option>Unir</option>
+					<option>Alterar dono</option>
+					<option>Enviar email</option>
+					<option>Exportar</option>
+				</select>
+			</p>
+			
+			<h1>Clientes que receberam ficha</h1>
+			<table cellpadding="0" cellspacing="0" border="0" class="tabela">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Nome</th>
+						<th>Email</th>
+						<th>Telefone</th>
+						<th>Resp.</th>
+						<th>Status</th>
+						<th>Interesses</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>412, 444</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Janaína</td>
+						<td>Novo</td>
+						<td>450, 451, 452</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Zé Genoíno</a></td>
+						<td>ze@genoino.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Cotinha</td>
+						<td>Novo</td>
+						<td>412, 444, 490</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+					<tr>
+						<td><input type="checkbox" /></td>
+						<td><a href="cliente.php">Maria Clarice</a></td>
+						<td>mama@mac.com</td>
+						<td>(11) 2125-4747</td>
+						<td>Vampeta</td>
+						<td>Novo</td>
+						<td>450, 451</td>
+					</tr>
+				</tbody>
+			</table>
+			<p class="acoes">
+				<label>Ações: </label>
+				<select>
+					<option>Unir</option>
+					<option>Alterar dono</option>
+					<option>Enviar email</option>
+					<option>Exportar</option>
+				</select>
+			</p>
+		</div>
+		
+<?php require('sidebar.php'); ?>
+</div>
+
+<?php
+require('footer.php');
+?>
